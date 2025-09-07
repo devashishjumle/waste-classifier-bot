cee24f0f8984f7a3fdb5eb8b3d370df5cd36073c
Devashish Jumle <devashishjumle96@gmail.com>
2025-09-07 23:53:03 +0530

    waste_bot.py

--- waste_bot.py
+++ waste_bot.py
@@ -13,14 +13,17 @@ waste_data = {
 }
 
 def classify_waste(item):
-    return waste_data.get(item.lower().strip(), "❌ Category not found! Please enter valid waste name.")
+    return waste_data.get(item.lower().strip(), None)
 
 st.set_page_config(page_title="Waste Classifier Bot", page_icon="♻️")
 st.title("♻️ Waste Classifier Chatbot")
-st.write("Enter a waste item name to know whether it is **Dry, Wet, or Hazardous**.")
+st.write("Type a waste item to know whether it is **Dry, Wet, or Hazardous**.")
 
 user_input = st.text_input("👉 Enter Waste Item:")
 
 if user_input:
     category = classify_waste(user_input)
-    st.success(f"🔎 Category: {category}")
+    if category:
+        st.success(f"🗑️ Item: **{user_input.capitalize()}** \n\n🔎 Category: **{category}**")
+    else:
+        st.error("❌ Item not found in database! Please try another waste name.")
